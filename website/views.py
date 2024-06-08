6bd96330b814184e23f08279066bc729e314064f
Igor Silva <33427463+guitoaraujo@users.noreply.github.com>
2024-06-09 00:08:55 +0100

    feat: Add authentication

--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def home(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'AIEEEEEEE')
+			return redirect('home')
+		else:
+			messages.success(request, 'BOOOOOOOOOH')
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+		logout(request)
+		messages.success(request, 'BYE')
+		return redirect('home')
